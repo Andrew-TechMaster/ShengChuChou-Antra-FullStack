a1fd4b46d24e7b85448ff237ee04ec04dfbee702
Sheng-Chu Chou <andrewchou1996@gmail.com>
2021-09-21 17:36:49 -0700

    Add MovieShop Folder

---
+++ MovieShop_Andrew/Infrastructure/Services/MovieService.cs
@@ -0,0 +1,31 @@
+﻿using ApplicationCore.Models;
+using ApplicationCore.ServiceInterfaces;
+using Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Services
+{
+    public class MovieService : IMovieService
+    {
+        public IEnumerable<MovieCardResponseModel> Get30HighestGrossingMovies()
+        {
+            MovieRepository repo = new MovieRepository();
+
+            // list of movie entities
+            var movies = repo.Get30HighestGrossingMovies();
+
+            // mapping entities to models data so that services always return models not entities
+            var moviesCardResponseModel = new List<MovieCardResponseModel>();
+            foreach (var movie in movies)
+            {
+                moviesCardResponseModel.Add(new MovieCardResponseModel { Id = movie.Id, PosterUrl = movie.PosterUrl });
+            }
+
+            return moviesCardResponseModel;
+        }
+    }
+}
---
+++ MovieShop_Andrew/MovieShopMVC/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using Infrastructure.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MovieShopMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieShopMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+           
+        }
+
+        public IActionResult Index()
+        {
+            // it will look inside views folder => folder name with sae name as controller name and look for the view with the same name as action method name
+            // get model data (top 30 revenue)
+            var movieService = new MovieService();
+            var movies = movieService.Get30HighestGrossingMovies();
+            return View(movies);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
